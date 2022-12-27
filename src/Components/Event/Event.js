b827957e47a482b90ca12b4c8f0fabff9521990b
codinghuntsman <sajedulislam.cse@gmail.com>
2022-12-27 22:28:26 +0600

    event section code added

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -1,10 +1,53 @@
 import React from "react";
 import style from "./Event.module.css";
+import img from "../../../src/images/halfImg.jpg";
+import data from "./EventData";
+import { useState } from "react";
+import { AiOutlineFieldTime } from "react-icons/ai";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 
 const Event = () => {
+  const [allData] = useState(data);
   return (
     <div className={style.eventMainDiv}>
-      <h1>Hello Event</h1>
+      <div className={style.imageDiv}>
+        <div className={style.overlay}>
+          <h1 className={style.eventText}>OUR EVENT</h1>
+        </div>
+        <img className={style.image} src={img} alt="avatar" />
+      </div>
+      <div className={style.EventMapMainDiv}>
+        {allData.map((item) => (
+          <div className={style.AllContentDiv}>
+            <div className={style.EventImageDiv}>
+              <p className={style.Date}>{item.date}</p>
+              <img className={style.eventImg} src={item.img} alt="avatar" />
+            </div>
+            <div className={style.SubContentDiv}>
+              <h2 className={style.eventTitle}>{item.title}</h2>
+              <p className={style.eventTime}>
+                {" "}
+                <span>{<AiOutlineFieldTime className={style.EventTimeIcon} />}</span> {item.time}
+              </p>
+              <p className={style.eventLocation}>
+                {" "}
+                <span>{<HiOutlineLocationMarker className={style.EventLocationIcon} />}</span>
+                {item.location}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={style.DonateSection}>
+        <div className={style.DonateButtonDiv}>
+          <h1 className={style.HelpText}>
+            Help For Poor <br /> people Worldwide
+          </h1>
+          <button className={style.DonateButton}>
+            <a href="/#">Donate Please</a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/Event/EventData.js
@@ -0,0 +1,45 @@
+const data = [
+  {
+    img: "images/eventImg1.jpg",
+    title: "Mission for Fresh & clean Water",
+    time: "08 to 10PM",
+    date: "12 Dec",
+    location: "Jordan house, Bafelo",
+  },
+  {
+    img: "images/eventImg2.jpg",
+    title: "Education for poor children",
+    time: "05 to 11PM",
+    date: "14 Dec",
+    location: "Jordan house, new York",
+  },
+  {
+    img: "images/eventImg3.jpg",
+    title: "Education for street children",
+    time: "10 to 12PM",
+    date: "15 Dec",
+    location: "Jordan house, Canada",
+  },
+  {
+    img: "images/eventImg4.jpg",
+    title: "Rights for poor children",
+    time: "01 to 10PM",
+    date: "16 Dec",
+    location: "Jordan house, Jorzia",
+  },
+  {
+    img: "images/eventImg5.jpg",
+    title: "Education for poor children",
+    time: "08 to 10AM",
+    date: "17 Dec",
+    location: "New York City, Montaria",
+  },
+  {
+    img: "images/eventImg6.jpg",
+    title: "Education for poor children",
+    time: "08 to 10PM",
+    date: "23 Dec",
+    location: "Jordan house, India",
+  },
+];
+export default data;
