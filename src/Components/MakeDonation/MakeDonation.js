3fb48b67919c51009ecfa470115fc55055739d04
codinghuntsman <sajedulislam.cse@gmail.com>
2022-12-29 17:50:28 +0600

    donate code added

--- src/Components/MakeDonation/MakeDonation.js
+++ src/Components/MakeDonation/MakeDonation.js
@@ -1,10 +1,21 @@
 import React from "react";
 import style from "./MakeDonation.module.css";
+import donateImage from "../../../src/images/donateImg.jpg";
 
 const MakeDonation = () => {
   return (
     <div className={style.MakeDonationMainDiv}>
-      <h1>Hey,,, Now you are makeDonation section</h1>
+      <div className={style.ImageDiv}>
+        <div className={style.overlay}>
+          <h1 className={style.AppreciateText}>We Appreciate You For Your Kindness</h1>
+
+          <div className={style.DonateButtonDiv}>
+            <input className={style.DonateInput} type="number" placeholder="$Put value" />
+            <button className={style.DonateButton}>Donate</button>
+          </div>
+        </div>
+        <img className={style.DonateImg} src={donateImage} alt="avatar" />
+      </div>
     </div>
   );
 };
