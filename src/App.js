cdc8101c5c1ea1b0984227c87f944970dc055056
codinghuntsman <sajedulislam.cse@gmail.com>
2022-12-29 18:13:26 +0600

    redirect code added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import MakeDonation from "./Components/MakeDonation/MakeDonation";
 import Navbar from "./Components/Navbar/Navbar";
+import NotFound from "./Components/NotFound/NotFound";
 import Service from "./Components/Service/Service";
 import TopNav from "./Components/TopNav/TopNav";
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/event" element={<Event></Event>}></Route>
         <Route path="/contact" element={<Contact></Contact>}></Route>
         <Route path="/donate" element={<MakeDonation></MakeDonation>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className={style.NotFound}>Opps!! The page you are looking that not found !</h1>
+    </div>
+  );
+};
+
+export default NotFound;
